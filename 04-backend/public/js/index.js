f4dfb2208220d01ceee68ada540072d8bc81ceea
marciojrtorres <marciojrtorres@gmail.com>
2017-09-25 15:57:54 -0300

    upando

--- 04-backend/public/js/index.js
+++ 04-backend/public/js/index.js
@@ -1,1 +1,15 @@
 console.log('ok');
+const provider = new firebase.auth.GoogleAuthProvider();
+
+document.querySelector('div.naologado a')
+    .addEventListener('click', function() {
+    firebase.auth().signInWithRedirect(provider);
+});
+
+firebase.auth()
+.getRedirectResult().then(function(result) {
+    let user = result.user;
+    console.log(user);
+}).catch(function(error) {
+    console.log(error);
+});
