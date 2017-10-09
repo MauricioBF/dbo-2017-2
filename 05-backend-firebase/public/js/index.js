0c492654ba80fec27e106ed2298192bd4215c694
marciojrtorres <marciojrtorres@gmail.com>
2017-10-09 16:31:28 -0300

    login, database

--- 05-backend-firebase/public/js/index.js
+++ 05-backend-firebase/public/js/index.js
@@ -1,28 +1,68 @@
 /* global firebase */
-
 console.log('ok');
 const provider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.database();
 
 const linkLogin = 
     document.querySelector('div.naologado a');
+const linkLogout = document.querySelector('div.logado a');
 const avatar = document.querySelector('div.logado img');
 const nome = document.querySelector('div.logado span');
+const divLogado = document.querySelector('div.logado');
+const divNaoLogado = document.querySelector('div.naologado');
+const form = document.querySelector('form');
+const divAvisos = document.querySelector('div.avisos');
+
+form.addEventListener('submit', function(e) {
+    if (usuario) {
+        db.ref('/avisos').push({
+            texto: this.texto.value
+        });
+        this.texto.value = '';
+    } else {
+        // TODO: avisar que é necessário logar-se
+    }
+    e.preventDefault();
+});
+
+db.ref('/avisos').on('child_added', function(aviso) {
+    const p = document.createElement('p');
+    p.textContent = aviso.val().texto;
+    divAvisos.appendChild(p);
+});
+
+linkLogout.addEventListener('click', function() {
+    firebase.auth().signOut();
+});
 
 linkLogin.addEventListener('click', function() {
     firebase.auth().signInWithRedirect(provider);
 });
 
-firebase.auth()
-.getRedirectResult().then(function(result) {
-    if ( ! result.user) return;
-    const user = result.user;    
-    avatar.src = user.photoURL;
-    
-    .textContent = user.displayName;
-    document.querySelector('div.logado')
-    .style.display = 'block';
-    document.querySelector('div.naologado')
-    .style.display = 'none';
-}).catch(function(error) {
-    alert(error.message);
+let usuario = null;
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        usuario = user;
+        divLogado.style.display = 'block';
+        divNaoLogado.style.display = 'none'; 
+        avatar.src = user.photoURL;
+        nome.textContent = user.displayName;    
+    } else {
+        usuario = null;
+        divLogado.style.display = 'none';
+        divNaoLogado.style.display = 'block';
+        avatar.src = '';
+        nome.textContent = '';
+    }
 });
+
+
+
+
+
+
+
+
+
+
